4505f17c03649133133742495e4bb8302399a970
Schmid Lothar <web@jagolo.ch>
2022-04-29 05:59:24 +0200

    Standart

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/majors-suite", handlers.Repo.Major)
+	mux.Get("/generals-quarters", handlers.Repo.General)
+	mux.Get("/search-avalability", handlers.Repo.Avalability)
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -11,6 +11,7 @@ import (
 // TemplateData holds data send from handlers to templates
 
 var Repo *Repository
+
 type Repository struct {
 	App *config.AppConfig
 }
@@ -30,17 +31,38 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
-	
+
 }
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hallo Lothar"
 
 	remotIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	
+
 	stringMap["remote_ip"] = remotIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["name"] = "Schmid Lothar"
+	stringMap["mail"] = "jagolo@jagolo.ch"
+
+	remotIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remotIP
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Major(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) General(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Avalability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-avalaible.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
 }
